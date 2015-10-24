d7d22b2c04a8a1cafa364e8d3c3d934020e82cba
javaadvanceprogramming@gmail.com <admin123##>
2015-10-24 10:49:15 +0700

    refact constructor

--- EmployeeManagementSystem/app.js
+++ EmployeeManagementSystem/app.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-// var employee = require('./routes/employee_mysql');
-var employee = require('./routes/employee_cloudant');
+var employee = require('./routes/employee');
 var catalog = require('./routes/catalog');
 
 var app = express();
@@ -10,7 +9,6 @@ app.use(bodyParser.json());
 app.use(app.router);
 app.use(express.errorHandler());
 
-// var db = catalog.getMySqlDb();
 var db = catalog.getCloudantDb();
 
 // define services
@@ -18,8 +16,11 @@ app.get('/find/all', employee.findAll(db));
 app.get('/find/:employeeId', employee.findByEmployeeId(db));
 app.post('/add', employee.add(db));
 
-// start server
+// the IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts
+// this application:
 var host = (process.env.VCAP_APP_HOST || 'localhost');
+// the port on the DEA for communication with the application:
 var port = (process.env.VCAP_APP_PORT || 3000);
+// start server
 app.listen(port, host);
 console.log('App started on port ' + port);
--- EmployeeManagementSystem/routes/catalog.js
+++ EmployeeManagementSystem/routes/catalog.js
@@ -1,3 +1,11 @@
+// There are many useful environment variables available in process.env,
+// please refer to the following document for detailed description:
+// http://ng.w3.bluemix.net/docs/FAQ.jsp#env_var
+
+// VCAP_SERVICES contains all the credentials of services bound to
+// this application. For details of its content, please refer to
+// the document or sample of each service.
+
 exports.getMySqlDb = function() {
 	if (process.env.VCAP_SERVICES) {
 		var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
@@ -22,9 +30,11 @@ exports.getMySqlDb = function() {
 
 exports.getCloudantDb = function() {
 	if (process.env.VCAP_SERVICES) {
+		// get service credentials and communicate with bluemix services.
 		var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
 		for ( var svcName in services) {
 			if (svcName.match(/^cloudantNoSQLDB/)) {
+				// add a Cloudant NoSQL DB service.
 				var cloudant = services[svcName][0].credentials;
 
 				var nano = require('nano')(cloudant.url);
